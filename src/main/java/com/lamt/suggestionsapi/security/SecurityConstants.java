32a955bbb8eabcaa20d57167f8d03ae7bb30f374
Adawari Thomas <adawari.thomas@bjss.com>
2024-08-26 13:27:42 +0100

    Revert "remove security on root"
    This reverts commit 09b087f9570fe7c25be6e98bed995f180a444116.

--- src/main/java/com/lamt/suggestionsapi/security/SecurityConstants.java
+++ src/main/java/com/lamt/suggestionsapi/security/SecurityConstants.java
@@ -2,7 +2,6 @@ package com.lamt.suggestionsapi.security;
 
 public class SecurityConstants {
     public static final String REGISTER_PATH = "/user/register";
-    public static final String HEALTH_CHECK_PATH = "/";
     public static final int EXPIRES_AT = 7200000;
     public static final String DURATION = "Duration";
     public static final String AUTHORIZATION = "Authorization";
